aae7611ffe50ee5a8b2f7295a10126bea8ecce42
jerems34 <jeremy.simione@gmail.com>
2021-03-30 11:46:13 +0200

    Design update

--- LivraisonRestaurant/app/src/main/java/com/example/livraisonrestaurant/ui/login/Auth/client.java
+++ LivraisonRestaurant/app/src/main/java/com/example/livraisonrestaurant/ui/login/Auth/client.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 
 import com.example.livraisonrestaurant.R;
 import com.example.livraisonrestaurant.ui.login.BaseActivity;
+import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 public class client extends BaseActivity {
-
-
 
 
 
