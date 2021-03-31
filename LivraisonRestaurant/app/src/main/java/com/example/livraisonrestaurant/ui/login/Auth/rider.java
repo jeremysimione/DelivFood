2541832a8c93adbf9773aba240c770a8696aac0d
jerems34 <jeremy.simione@gmail.com>
2021-03-31 14:05:50 +0200

    Add design for rider

--- LivraisonRestaurant/app/src/main/java/com/example/livraisonrestaurant/ui/login/Auth/rider.java
+++ LivraisonRestaurant/app/src/main/java/com/example/livraisonrestaurant/ui/login/Auth/rider.java
@@ -1,14 +1,26 @@
 package com.example.livraisonrestaurant.ui.login.Auth;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.livraisonrestaurant.R;
 import com.example.livraisonrestaurant.ui.login.BaseActivity;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,19 +28,40 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
-public class rider extends AppCompatActivity implements OnMapReadyCallback {
-
+public class rider extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener,
+        OnMapReadyCallback {
+    private View locationButton;
+    private GoogleMap mMap;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rider);
+        //ConstraintLayout cl = findViewById(R.id.myconstraintlayout);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton14);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View v) {
+                DrawerLayout mDrawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+                mDrawerLayout.openDrawer(Gravity.START);
+            }
+        });
+
+
     }
+
+    @SuppressLint("ResourceType")
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        mMap=googleMap;
+        mMap.setMyLocationEnabled(true);
         try {
             // Customise the styling of the base map using a JSON object defined
             // in a raw resource file.
@@ -37,12 +70,44 @@ public class rider extends AppCompatActivity implements OnMapReadyCallback {
                             this, R.raw.mymap));
 
             if (!success) {
-                System.out.println( "Style parsing failed.");
+                System.out.println("Style parsing failed.");
             }
         } catch (Resources.NotFoundException e) {
-            System.out.println( "Can't find style. Error: " +  e);
+            System.out.println("Can't find style. Error: " + e);
         }
-        // Position the map's camera near Sydney, Australia.
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-34, 151)));
+        // get your maps fragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        // Extract My Location View from maps fragment
+        locationButton = mapFragment.getView().findViewById(0x2);
+        // Change the visibility of my location button
+        if(locationButton != null)
+            locationButton.setVisibility(View.GONE);
+
+        FloatingActionButton fa = findViewById(R.id.floatingActionButton12);
+        fa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMap != null) {
+                    if (locationButton != null)
+                        locationButton.callOnClick();
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT)
+                .show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
     }
 }
