bb075c7969c18bc2732d6dd81169a061c9f504d5
jerems34 <jeremy.simione@gmail.com>
2021-05-25 13:12:59 +0200

    fix some errors

--- LivraisonRestaurant/app/src/main/java/com/example/livraisonrestaurant/ui/login/Auth/Client/PaymentActivity.java
+++ LivraisonRestaurant/app/src/main/java/com/example/livraisonrestaurant/ui/login/Auth/Client/PaymentActivity.java
@@ -1,21 +1,17 @@
 package com.example.livraisonrestaurant.ui.login.Auth.Client;
 
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
-public class PaymentActivity extends WearableActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private TextView mTextView;
+import com.example.livraisonrestaurant.R;
 
+public class PaymentActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
-        setAmbientEnabled();
     }
 }
